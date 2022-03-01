042c5d38fd7899e934a6737e9caedbfa18308813
Bagus Rangin <brangin98@gmail.com>
2022-03-01 13:15:58 +0700

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './assets/styles.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import TourCard from './components/TourCard/TourCard';
+import Navbar from './components/Navbar/Navbar';
+import Band from './components/Band/Band';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Navbar/>
+    <Band/>
+  <div>
+    <TourCard/>
+    <TourCard/>
+    <TourCard/>
+  </div>
+  </>
   );
 }
 
---
+++ src/components/Band/BandPhotos.jsx
@@ -0,0 +1,14 @@
+import member from './member.jpeg'
+
+const BandPhotos = () => {
+    return (
+        
+        <div className="band-member-card">
+          <p>Name</p>
+          <img src={member}alt="" />
+        </div>
+     
+    )
+}
+
+export default BandPhotos;
